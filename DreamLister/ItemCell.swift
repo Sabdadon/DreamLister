d3c171cd10d7dd93017f63c6bb99573998a96388
Sabdadon <sabarish111095@gmail.com>
2017-03-11 15:53:19 +0530

    Linked the objects

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by sabarish sridhar on 11/03/17.
+//  Copyright © 2017 sabarish. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var details: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var thumb: UIImageView!
+    
+    @IBOutlet weak var title: UILabel!
+}
--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -8,12 +8,25 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var segment: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0;
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    
 }
 
+t
